ddf41e695851c47f626907efede1b52892741bd8
Paulson McIntyre <paul@gpmidi.net>
2021-10-14 20:59:15 -0400

    Changing active default to default to enabled

--- ffstream/admin.py
+++ ffstream/admin.py
@@ -1,13 +1,44 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
 from .models import *
+
+
+class ActiveBooleanDefault(SimpleListFilter):
+    # title = ''
+    parameter_name = 'active'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('all', 'All'),
+            (1, 'Yes'),
+            (None, 'No')
+        )
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == (str(lookup) if lookup else lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}, []),
+                'display': title,
+            }
+
+    def queryset(self, request, queryset):
+        if self.value():
+            if self.value() == "all":
+                return queryset
+            else:
+                return queryset.filter(**{self.parameter_name: self.value()})
+        elif self.value() is None:
+            return queryset.filter(**{self.parameter_name: False})
 
 
 class KeyAdmin(admin.ModelAdmin):
     date_hierarchy = "modified"
     list_filter = (
         "is_live",
-        "active",
+        #"active",
+        ActiveBooleanDefault,
         "livestream",
         "pull",
     )
@@ -35,7 +66,7 @@ class KeyAdmin(admin.ModelAdmin):
     search_fields = (
         "name",
         "id",
-        )
+    )
 
 
 # Register your models here.
