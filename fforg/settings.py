74348d8d015829d81942b8d3acde14812c0f9517
Paulson McIntyre <paul@gpmidi.net>
2024-01-21 15:13:04 -0500

    Switched to viewflow OSS https://viewflow.io/

--- fforg/settings.py
+++ fforg/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'memoize',
-    "django_workflow_engine",
+    "viewflow",
     "oauth2_provider",
     'ffsite',
     'ffsfdc',
