c3845c54148d49553f3dc4fdcb020ab6d7b85f35
Aevum <aevum@decess.us>
2026-04-13 13:55:53 -0400

    Add tests for Discord credential validators and login button visibility

--- ffsite/tests.py
+++ ffsite/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 
 
@@ -10,3 +10,22 @@ class LoginErrorViewTest(TestCase):
     def test_contains_discord_invite_link(self):
         response = self.client.get(reverse('login-error'))
         self.assertContains(response, 'discord.gg/fragforce')
+
+
+class DiscordLoginButtonTest(TestCase):
+    def setUp(self):
+        from django.core.cache import cache
+        cache.clear()
+
+    @override_settings(
+        SOCIAL_AUTH_DISCORD_KEY='123456789012345678',
+        SOCIAL_AUTH_DISCORD_SECRET='abcDEF123_-abcDEF123_-abcDEF1234',
+    )
+    def test_login_button_shown_when_credentials_valid(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, reverse('social:begin', args=['discord']))
+
+    @override_settings(SOCIAL_AUTH_DISCORD_KEY='', SOCIAL_AUTH_DISCORD_SECRET='')
+    def test_login_button_hidden_when_credentials_empty(self):
+        response = self.client.get(reverse('home'))
+        self.assertNotContains(response, reverse('social:begin', args=['discord']))
