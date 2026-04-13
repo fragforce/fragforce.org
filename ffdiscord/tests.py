48d0ff8a1b7e5f4c690c2f4de91cbd1aec36276d
Aevum <aevum@decess.us>
2026-04-13 15:53:22 -0400

    Add tests for ffdiscord utils: sync_guild_roles and sync_user_roles

--- ffdiscord/tests.py
+++ ffdiscord/tests.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import Group, User
 from django.test import TestCase
 
+from ffdiscord.models import DiscordRole, DiscordRoleMapping
+from ffdiscord.utils import sync_guild_roles, sync_user_roles
 from ffdiscord.validators import (
     discord_bot_token_valid,
     discord_oauth_credentials_valid,
@@ -75,3 +78,76 @@ class DiscordBotTokenValidTest(TestCase):
 
     def test_missing_dots(self):
         self.assertFalse(discord_bot_token_valid('M' + 'A' * 70))
+
+
+class SyncGuildRolesTest(TestCase):
+    def test_creates_new_roles(self):
+        sync_guild_roles([('111', 'Admin'), ('222', 'Streamer')])
+        self.assertEqual(DiscordRole.objects.count(), 2)
+        self.assertTrue(DiscordRole.objects.filter(discord_role_id='111', name='Admin').exists())
+
+    def test_updates_existing_role_name(self):
+        DiscordRole.objects.create(discord_role_id='111', name='OldName')
+        sync_guild_roles([('111', 'NewName')])
+        self.assertEqual(DiscordRole.objects.get(discord_role_id='111').name, 'NewName')
+
+    def test_handles_empty_list(self):
+        sync_guild_roles([])
+        self.assertEqual(DiscordRole.objects.count(), 0)
+
+    def test_upserts_multiple_roles(self):
+        DiscordRole.objects.create(discord_role_id='111', name='Existing')
+        sync_guild_roles([('111', 'Updated'), ('222', 'New')])
+        self.assertEqual(DiscordRole.objects.count(), 2)
+        self.assertEqual(DiscordRole.objects.get(discord_role_id='111').name, 'Updated')
+
+
+class SyncUserRolesTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='streamer')
+        self.role = DiscordRole.objects.create(discord_role_id='111', name='Streamer')
+        self.group = Group.objects.create(name='streamers')
+        self.mapping = DiscordRoleMapping.objects.create(role=self.role, group=self.group)
+
+    def test_adds_entitled_group(self):
+        sync_user_roles(self.user, ['111'])
+        self.assertIn(self.group, self.user.groups.all())
+
+    def test_removes_unentitled_group(self):
+        self.user.groups.add(self.group)
+        sync_user_roles(self.user, [])
+        self.assertNotIn(self.group, self.user.groups.all())
+
+    def test_no_change_when_correct(self):
+        self.user.groups.add(self.group)
+        sync_user_roles(self.user, ['111'])
+        self.assertIn(self.group, self.user.groups.all())
+
+    def test_early_return_when_no_mappings(self):
+        DiscordRoleMapping.objects.all().delete()
+        self.user.groups.add(self.group)
+        sync_user_roles(self.user, [])
+        # Should not remove group since there are no mappings to manage
+        self.assertIn(self.group, self.user.groups.all())
+
+    def test_grants_staff_access(self):
+        self.mapping.grants_staff_access = True
+        self.mapping.save()
+        sync_user_roles(self.user, ['111'])
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.is_staff)
+
+    def test_revokes_staff_access(self):
+        self.mapping.grants_staff_access = True
+        self.mapping.save()
+        self.user.is_staff = True
+        self.user.save()
+        self.user.groups.add(self.group)
+        sync_user_roles(self.user, [])
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_staff)
+
+    def test_staff_not_set_without_grants_staff_access(self):
+        sync_user_roles(self.user, ['111'])
+        self.user.refresh_from_db()
+        self.assertFalse(self.user.is_staff)
